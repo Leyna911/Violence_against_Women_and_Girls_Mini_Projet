335c81c655e7b8db4b48acc7bc72a9fdcecc9878
Leyna911 <yl.boudchicha@esi-sba.dz>
2024-01-08 10:43:13 +0100

    Apriori algorithm

--- ViolenceAgainstWomen.py
+++ ViolenceAgainstWomen.py
@@ -1,1 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, constr, conint
+
+
+class ViolenceAgainstWomen(BaseModel):
+    Country: constr(strip_whitespace=True)
+    Age:constr(strip_whitespace=True)
+    Education:	constr(strip_whitespace=True) 
+    MaritalStatus:	constr(strip_whitespace=True, alias="Marital Status")
+    Employment: constr(strip_whitespace=True)
+    Residence: constr(strip_whitespace=True) 
+    SurveyYear:	conint(ge=0, alias="Survey Year")
+    Question:constr(strip_whitespace=True)
---
+++ app.py
@@ -0,0 +1,7 @@
+import uvicorn
+from fastapi import FastAPI
+import joblib
+from CarUser import CarUser
+
+
+app = FastAPI()
